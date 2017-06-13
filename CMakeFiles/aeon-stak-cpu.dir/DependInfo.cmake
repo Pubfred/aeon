3d222d77604ea6eb884f13a58dd6e61bc7a5ce85
Pubfred <root@ip-172-31-6-238.us-east-2.compute.internal>
2017-06-13 19:04:30 +0000

    v2

---
+++ CMakeFiles/aeon-stak-cpu.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ubuntu/aeon/crypto/c_blake256.c" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/crypto/c_blake256.c.o"
+  "/home/ubuntu/aeon/crypto/c_groestl.c" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/crypto/c_groestl.c.o"
+  "/home/ubuntu/aeon/crypto/c_jh.c" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/crypto/c_jh.c.o"
+  "/home/ubuntu/aeon/crypto/c_keccak.c" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/crypto/c_keccak.c.o"
+  "/home/ubuntu/aeon/crypto/c_skein.c" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/crypto/c_skein.c.o"
+  "/home/ubuntu/aeon/crypto/soft_aes.c" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/crypto/soft_aes.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/aeon/cli-miner.cpp" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/cli-miner.cpp.o"
+  "/home/ubuntu/aeon/console.cpp" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/console.cpp.o"
+  "/home/ubuntu/aeon/crypto/cryptonight_common.cpp" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/crypto/cryptonight_common.cpp.o"
+  "/home/ubuntu/aeon/executor.cpp" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/executor.cpp.o"
+  "/home/ubuntu/aeon/httpd.cpp" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/httpd.cpp.o"
+  "/home/ubuntu/aeon/jconf.cpp" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/jconf.cpp.o"
+  "/home/ubuntu/aeon/jpsock.cpp" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/jpsock.cpp.o"
+  "/home/ubuntu/aeon/minethd.cpp" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/minethd.cpp.o"
+  "/home/ubuntu/aeon/socket.cpp" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/socket.cpp.o"
+  "/home/ubuntu/aeon/webdesign.cpp" "/home/ubuntu/aeon/CMakeFiles/aeon-stak-cpu.dir/webdesign.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/aeon-stak-cpu.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aeon-stak-cpu.dir/crypto/c_jh.c.o"
+  "CMakeFiles/aeon-stak-cpu.dir/crypto/soft_aes.c.o"
+  "CMakeFiles/aeon-stak-cpu.dir/crypto/c_skein.c.o"
+  "CMakeFiles/aeon-stak-cpu.dir/crypto/c_blake256.c.o"
+  "CMakeFiles/aeon-stak-cpu.dir/crypto/c_keccak.c.o"
+  "CMakeFiles/aeon-stak-cpu.dir/crypto/c_groestl.c.o"
+  "CMakeFiles/aeon-stak-cpu.dir/crypto/cryptonight_common.cpp.o"
+  "CMakeFiles/aeon-stak-cpu.dir/jconf.cpp.o"
+  "CMakeFiles/aeon-stak-cpu.dir/minethd.cpp.o"
+  "CMakeFiles/aeon-stak-cpu.dir/cli-miner.cpp.o"
+  "CMakeFiles/aeon-stak-cpu.dir/socket.cpp.o"
+  "CMakeFiles/aeon-stak-cpu.dir/webdesign.cpp.o"
+  "CMakeFiles/aeon-stak-cpu.dir/httpd.cpp.o"
+  "CMakeFiles/aeon-stak-cpu.dir/jpsock.cpp.o"
+  "CMakeFiles/aeon-stak-cpu.dir/executor.cpp.o"
+  "CMakeFiles/aeon-stak-cpu.dir/console.cpp.o"
+  "bin/aeon-stak-cpu.pdb"
+  "bin/aeon-stak-cpu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/aeon-stak-cpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
